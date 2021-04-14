98bbdfce0706e410cfc4b850c3cae743b76f747f
devchicala <delciofrancisco1@gmail.com>
2021-04-14 22:21:11 +0100

    add news config changes

---
+++ src/interface/app.utils.tsx
@@ -0,0 +1,14 @@
+interface cep {
+    bairro: string,
+    cep: string,
+    complemento: string,
+    ddd: string,
+    gia: string,
+    ibge: string,
+    localidade: string,
+    logradouro: string,
+    siafi: string
+    uf: string,
+}
+
+export default cep;
