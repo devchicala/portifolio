3459c59f8b947a85846abbf919c115360c02b762
devchicala <delciofrancisco1@gmail.com>
2021-04-14 20:56:50 +0100

    secound commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './styles/App.css';
+import * as ReactBootStrap from 'react-bootstrap'
+
+type Cep = {
+  bairro: string,
+  cep: string,
+  complemento: string,
+  ddd: string,
+  gia: string,
+  ibge: string,
+  localidade: string,
+  logradouro: string,
+  siafi: string
+  uf: string,
+}
+
 
 function App() {
+  let dados; 
+  let dado: any;
+
+  const [dadoss, setDadoss] = useState(dado);
+  const [loading, setLoading] = useState(false);
+
+  async function getData(value: any) {
+    const data = await fetch(`https://viacep.com.br/ws/${value}/json/`);
+    return await data.json()
+}
+
+  function onblur(ev: any) {
+    const { value } = ev.target;
+    const cep = value?.replace(/[^0-9]/g, '');
+
+    if (cep?.length !== 8 || cep === '') {
+      setLoading(false)
+      return;
+    }
+
+    (async () => {
+      dados = await getData(cep);
+      setLoading(true)
+      setDadoss(dados.localidade)
+  })();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="texto">Insira um cep válido!</p>
+      <input className="input" type="text" onChange={(ev) =>  onblur(ev)} />
+      <div className="cep">
+        
+        {loading ? 'ESTE CEP É VÁLIDO, LOCALIDADE: ' + dadoss : <ReactBootStrap.Spinner animation="border" />}
+
+      </div>
     </div>
   );
 }
