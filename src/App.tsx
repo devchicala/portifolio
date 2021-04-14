03ff1adb43ec7c6976f344e43ade42bbea9996b1
devchicala <delciofrancisco1@gmail.com>
2021-04-15 00:12:48 +0100

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   let dados: Cep;
   let dado: any;
 
-  const [datas, setDatas] = useState(dado);
+  const [datas, setDatas] = useState<Cep | null>(null);
   const [loading, setLoading] = useState(false);
 
   async function getData(value: any) {
@@ -27,17 +27,33 @@ function App() {
     (async () => {
       dados = await getData(cep);
       setLoading(true);
-      setDatas(dados.localidade);
+      setDatas(dados);
     })();
   }
 
   return (
     <div className="App">
       <p className="texto">Insert a cep valid!</p>
-      <input className="input" maxLength={8} type="text" onChange={(ev) => onblur(ev)} />
+      <input
+        className="input"
+        maxLength={8}
+        type="text"
+        onChange={(ev) => onblur(ev)}
+      />
+      <div className="cep"></div>
       <div className="cep">
-        {loading && datas !== 'undefined' ? (
-          "THE CEP LOCATE IS: " + datas
+        {loading && datas?.cep !== "undefined" ? (
+          <>
+            <b>CEP: </b> {datas?.cep} <br />
+            <b>BAIRRO: </b> {datas?.bairro} <br />
+            <b>COMPLEMENTO: </b> {datas?.complemento} <br />
+            <b>DDD: </b> {datas?.ddd} <br />
+            <b>GIA: </b> {datas?.gia} <br />
+            <b>IBGE: </b> {datas?.ibge} <br />
+            <b>LOCALIDADE: </b> {datas?.localidade} <br />
+            <b>SIAFI: </b> {datas?.siafi} <br />
+            <b>UF: </b> {datas?.uf} <br />
+          </>
         ) : (
           <ReactBootStrap.Spinner animation="border" />
         )}
